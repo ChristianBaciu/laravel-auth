93ea8d4cc190f1883543d14fcab78ebb1bed13c3
ChristianBaciu <christian3110@outlook.it>
2024-03-25 20:34:35 +0100

    foreach in index.blade

--- resources/views/pages/project/index.blade.php
+++ resources/views/pages/project/index.blade.php
@@ -2,5 +2,26 @@
 
 {{-- yield di app.blade --}}
 @section('content')
-    <h1>ciao mondo</h1>
+    <div class="container my-5">
+        <div class="table-responsive">
+            <table class="table table-secondary">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">TITOLO</th>
+                        <th scope="col">CONTENUTO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($project as $item)
+                        <tr>
+                            <td>{{$item->id}}</td>
+                            <td>{{$item->titolo}}</td>
+                            <td>{{$item->contenuto}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
